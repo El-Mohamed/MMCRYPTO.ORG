50f70d514d59b4c1af7627baf44d0af7c620d050
Mohamed <elmoufid.mohamed@outlook.com>
2020-03-23 21:43:06 +0100

    Add Coin Controller
    + Removed Weather Controller

---
+++ MM-Crypto/MM-Crypto/Controllers/CoinController.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MM_Crypto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CoinController : ControllerBase
+    {
+    }
+}
