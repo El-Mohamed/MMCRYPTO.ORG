b8996ae01ba83d371846f1ccf3570b806e8db636
Mohamed <elmoufid.mohamed@outlook.com>
2020-03-23 21:46:15 +0100

    Add DbContext

---
+++ MM-Crypto/MM-Crypto/Database/CryptoContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MM_Crypto
+{
+    public class CryptoContext: DbContext
+    {
+        public CryptoContext(DbContextOptions<CryptoContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Coin> Coins { get; set; }
+        public DbSet<Founder> Founders { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
+    }
+}
