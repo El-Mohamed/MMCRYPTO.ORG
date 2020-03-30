9d354ee9d8bb20483df0145473f505b3ecc67690
Mohamed <elmoufid.mohamed@outlook.com>
2020-03-30 19:15:01 +0200

    Refactor Price Table + Add Sort Buttons

--- Web Application/MM-Crypto/src/app/components/price/price.component.ts
+++ Web Application/MM-Crypto/src/app/components/price/price.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { CoincapService, CoinCapAsset } from 'src/app/services/coincap.service';
+import { SortEvent } from 'primeng/api';
 
 @Component({
   selector: 'app-price',
@@ -9,6 +10,14 @@ import { CoincapService, CoinCapAsset } from 'src/app/services/coincap.service';
 export class PriceComponent implements OnInit {
 
   AllCoinCapAssets: CoinCapAsset[] = [];
+
+  Columns = [
+    { field: 'rank', header: 'Rank' },
+    { field: 'name', header: 'Name' },
+    { field: 'price', header: 'Price' },
+    { field: 'volumeUsd24Hr', header: 'Volume' },
+    { field: 'changePercent24Hr', header: 'Change' }
+  ];
 
   constructor(private service: CoincapService) {
 
@@ -21,4 +30,24 @@ export class PriceComponent implements OnInit {
 
   ngOnInit(): void { }
 
+  customSort(event: SortEvent) {
+    console.log("sorting...")
+    event.data.sort((data1, data2) => {
+      let value1 = data1[event.field];
+      let value2 = data2[event.field];
+      let result = null;
+
+      if (value1 == null && value2 != null)
+        result = -1;
+      else if (value1 != null && value2 == null)
+        result = 1;
+      else if (value1 == null && value2 == null)
+        result = 0;
+      else if (typeof value1 === 'string' && typeof value2 === 'string')
+        result = value1.localeCompare(value2);
+      else
+        result = (value1 < value2) ? -1 : (value1 > value2) ? 1 : 0;
+      return (event.order * result);
+    });
+  }
 }
