9864b138db9f493ebfa75159d2db1006eaa74edc
Mohamed <elmoufid.mohamed@outlook.com>
2020-04-05 01:36:56 +0200

    Add Founders Controller & GetAllFounders

---
+++ REST API/MM-Crypto/MM-Crypto/Controllers/FoundersController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MM_Crypto.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class FoundersController : ControllerBase
+    {
+        private readonly CryptoContext context;
+
+        public FoundersController(CryptoContext context)
+        {
+            this.context = context;
+        }
+
+        [HttpGet]
+        public List<Founder> GetAllFounders(  int? page, int lenght = 2)
+        {
+            IQueryable<Founder> query = context.Founders;
+
+            if (page.HasValue)
+                query = query.Skip(page.Value * lenght);
+            
+            query = query.Take(lenght);
+
+            return query.ToList();
+        }
+
+
+    }
+}
