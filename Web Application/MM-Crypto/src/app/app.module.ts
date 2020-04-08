2ccdaf964edc5e3184b0871b3c54498eeddd5c3e
Mohamed <elmoufid.mohamed@outlook.com>
2020-04-08 22:19:57 +0200

    Refactor app.module.ts

--- Web Application/MM-Crypto/src/app/app.module.ts
+++ Web Application/MM-Crypto/src/app/app.module.ts
@@ -1,31 +1,34 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 
+// Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { TabMenuModule } from 'primeng/tabmenu';
-import { NavigationComponent } from './components/navigation/navigation.component';
-import { HttpClientModule } from '@angular/common/http';
-import { TableModule } from 'primeng/table';
-import { RouterModule } from '@angular/router';
-import { ProgressBarModule } from 'primeng/progressbar';
-import { DropdownModule } from 'primeng/dropdown';
-import { FormsModule } from '@angular/forms';
-import { SpinnerModule } from 'primeng/spinner';
-import { InputTextModule } from 'primeng/inputtext';
-import { ButtonModule } from 'primeng/button';
-import { WalletComponent } from './components/wallet/wallet.component';
-import { DataViewModule } from 'primeng/dataview';
-import { ChartModule } from 'primeng/chart';
-import { NotFoundComponent } from './components/not-found/not-found.component';
-import { HomeComponent } from './components/home/home.component';
-import { NgApexchartsModule } from "ng-apexcharts";
 import { InformationComponent } from './components/information/information.component';
 import { FormComponent } from './components/form/form.component';
 import { PriceChartComponent } from './components/price-chart/price-chart.component';
 import { PriceTableComponent } from './components/price-table/price-table.component';
 import { CurrencyCalculatorComponent } from './components/currency-calculator/currency-calculator.component';
+import { NavigationComponent } from './components/navigation/navigation.component';
+import { WalletComponent } from './components/wallet/wallet.component';
+import { NotFoundComponent } from './components/not-found/not-found.component';
+import { HomeComponent } from './components/home/home.component';
+
+// Prime NG
+import { TableModule } from 'primeng/table';
+import { ProgressBarModule } from 'primeng/progressbar';
+import { DropdownModule } from 'primeng/dropdown';
+import { SpinnerModule } from 'primeng/spinner';
+import { InputTextModule } from 'primeng/inputtext';
+import { ButtonModule } from 'primeng/button';
+import { DataViewModule } from 'primeng/dataview';
+
+// Other
+import { NgApexchartsModule } from "ng-apexcharts";
 
 
 @NgModule({
@@ -46,18 +49,18 @@ import { CurrencyCalculatorComponent } from './components/currency-calculator/cu
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    TabMenuModule,
     HttpClientModule,
-    TableModule,
+    FormsModule,
     RouterModule,
+    // PrimeNG
+    TableModule,
     ProgressBarModule,
     DropdownModule,
-    FormsModule,
     SpinnerModule,
     InputTextModule,
     ButtonModule,
     DataViewModule,
-    ChartModule,
+    // Other
     NgApexchartsModule
   ],
   providers: [],
