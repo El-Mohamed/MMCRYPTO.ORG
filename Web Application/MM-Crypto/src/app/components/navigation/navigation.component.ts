9700bed90b567c07c1aa50b2165dfb25461e0cef
Mohamed <elmoufid.mohamed@outlook.com>
2020-04-25 01:43:09 +0200

    Add Login/Logout Button To Navigation

--- Web Application/MM-Crypto/src/app/components/navigation/navigation.component.ts
+++ Web Application/MM-Crypto/src/app/components/navigation/navigation.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { AuthService } from 'src/app/services/auth/auth.service';
 
 @Component({
   selector: 'app-navigation',
@@ -8,7 +9,7 @@ import { Component, OnInit } from '@angular/core';
 export class NavigationComponent implements OnInit
 {
 
-  constructor() { }
+  constructor(public auth: AuthService) { }
 
   ngOnInit(): void { }
 
