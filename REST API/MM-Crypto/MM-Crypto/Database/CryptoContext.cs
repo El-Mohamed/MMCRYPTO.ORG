9bffc2f5ff0c466de4cfa894f4207770cb6ac4f5
Mohamed <elmoufid.mohamed@outlook.com>
2020-04-30 09:54:12 +0200

    Add Many-To-Many Relation To DbContext

--- REST API/MM-Crypto/MM-Crypto/Database/CryptoContext.cs
+++ REST API/MM-Crypto/MM-Crypto/Database/CryptoContext.cs
@@ -4,13 +4,34 @@ namespace MM_Crypto
 {
     public class CryptoContext : DbContext
     {
+        public DbSet<Coin> Coins { get; set; }
+        public DbSet<Founder> Founders { get; set; }
+        public DbSet<Wallet> Wallets { get; set; }
+        public DbSet<WalletCoin> WalletCoin { get; set; }
+
         public CryptoContext(DbContextOptions<CryptoContext> options) : base(options)
         {
 
         }
 
-        public DbSet<Coin> Coins { get; set; }
-        public DbSet<Founder> Founders { get; set; }
-        public DbSet<Wallet> Wallets { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            // Many-to-many relationships
+
+            modelBuilder.Entity<WalletCoin>()
+                 .HasKey(w => new { w.WalletId, w.CoinId });
+
+            modelBuilder.Entity<WalletCoin>()
+                .HasOne(pt => pt.Wallet)
+                .WithMany(p => p.SupportedCoins)
+                .HasForeignKey(pt => pt.WalletId);
+
+            modelBuilder.Entity<WalletCoin>()
+                .HasOne(pt => pt.Coin)
+                .WithMany(t => t.SupportedWallets)
+                .HasForeignKey(pt => pt.CoinId);
+        }
     }
 }
---
+++ REST API/MM-Crypto/MM-Crypto/Models/WalletCoin.cs
@@ -0,0 +1,14 @@
+﻿namespace MM_Crypto
+{
+    /// <summary>
+    /// Entity class to represent the join table
+    /// </summary>
+
+    public class WalletCoin
+    {
+        public int WalletId { get; set; }
+        public int CoinId { get; set; }
+        public Coin Coin { get; set; }
+        public Wallet Wallet { get; set; }
+    }
+}
