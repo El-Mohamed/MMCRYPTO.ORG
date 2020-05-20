1f7a9d6f47ff26e4517f8c3d4f4803e222d4321f
Mohamed <elmoufid.mohamed@outlook.com>
2020-05-20 17:25:54 +0200

    Implemented Token Generator

--- Web Application/MM-Crypto/src/app/components/token-genrator/token-genrator.component.ts
+++ Web Application/MM-Crypto/src/app/components/token-genrator/token-genrator.component.ts
@@ -1,15 +1,38 @@
 import { Component, OnInit } from '@angular/core';
+import { MmCryptoAuthService, Auth0Token } from 'src/app/services/mm-crypto-auth/mm-crypto-auth.service';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-token-genrator',
   templateUrl: './token-genrator.component.html',
   styleUrls: ['./token-genrator.component.css']
 })
-export class TokenGenratorComponent implements OnInit {
+export class TokenGenratorComponent implements OnInit
+{
+  GeneratedToken: Auth0Token = {
+    access_token: "",
+    expires_in: 0,
+    token_type: ""
+  };
 
-  constructor() { }
+  constructor(private apiAuth: MmCryptoAuthService) { }
 
-  ngOnInit(): void {
+  ngOnInit(): void
+  {
   }
 
+  GenerateToken()
+  {
+    this.apiAuth.RequestNewToken().subscribe(
+      (data: Auth0Token) =>
+      {
+        this.GeneratedToken = data;
+      },
+      (error: HttpErrorResponse) =>
+      {
+        console.log(error);
+        alert("Error");
+      }
+    );
+  }
 }
